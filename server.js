982e1a77a3cc12685b3e905d1accbcbc58c672e4
Alex <agordon1123@gmail.com>
2019-09-09 16:44:21 -0400

    created accounts router

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require('express');
-
-const db = require('./data/dbConfig.js');
+const accountsRouter = require('./api/routers/accountsRouter');
 
 const server = express();
 
 server.use(express.json());
 
-server.get('/accounts', (req, res) => {
-    db('accounts')
-        .then(accounts => res.status(200).json(accounts))
-        .catch(err => res.status(500).json(err))
-});
+server.use('/api/accounts', accountsRouter);
 
 module.exports = server;
