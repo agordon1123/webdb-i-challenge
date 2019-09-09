e2f716069f0329acd89de0d260c6975757978397
Alex <agordon1123@gmail.com>
2019-09-09 16:35:10 -0400

    completed queries with stretch

--- server.js
+++ server.js
@@ -6,4 +6,10 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/accounts', (req, res) => {
+    db('accounts')
+        .then(accounts => res.status(200).json(accounts))
+        .catch(err => res.status(500).json(err))
+});
+
 module.exports = server;
